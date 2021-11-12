8ff03821a50947155e5a911de14d19f530ea9a4e
AJ <aj@smartnvm.com>
2021-11-12 13:05:51 -0500

    - add dashboard poduct/category counts

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,5 +1,7 @@
 class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with name: ENV['ADMIN_USERNAME'].to_s, password: ENV['ADMIN_PASSWORD'].to_s
   def show
+    @total_products = Product.count 
+    @total_categories = Category.count
   end
 end
